a9909c91cd16ae8d693ab320a66400574683df41
Anwar Ul Huda <anwar.h@seanergydigital.com>
2022-06-27 16:53:35 +0530

    Array accessing through index

---
+++ Ruby Programming/array_index.rb
@@ -0,0 +1,48 @@
+arr=[1,2,3,4,5,6,7,8,9]
+print arr
+puts "\n"
+
+#last index element
+print arr[-1]
+
+puts "\n"
+
+#first element
+print arr.first
+
+puts "\n"
+
+#last element
+print arr.last
+
+puts "\n"
+
+#print first n elements
+print arr.take(3)
+
+puts "\n"
+
+#insert 1 element
+print arr.push(10)
+
+puts "\n"
+
+#delete last element
+arr.pop()
+print arr
+
+puts "\n"
+
+#delete first element
+arr.shift
+print arr
+
+puts "\n"
+
+#insert element infront of array
+print arr.unshift(1)
+
+puts "\n"
+
+#select element after first n elements
+print arr.drop(3)
